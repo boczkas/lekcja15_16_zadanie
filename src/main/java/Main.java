429665a72e2807b2d98a2b5164b0cc64edd09114
Przemek <przemyslawjakubowski@gmail.com>
2022-09-15 16:24:13 +0200

    Solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 import klient.Klient;
 import klient.TypKlienta;
 import sklep.Sklep;
+import sklep.ZlodziejWSklepieException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// Na konsoli ma się pojawić:
+//  Witaj drogi Przemek
+//  Witaj drogi Tomek
+//  Otrzymujesz znizke 5%!
+//  Zlodziej w sklepie! Ewakuacja!
 public class Main {
     public static void main(String[] args) {
         List<Klient> klienci = new ArrayList<>();
@@ -15,6 +21,10 @@ public class Main {
         Sklep sklep = new Sklep();
 
 //        Napraw!
-        sklep.obsluzKlientow(klienci);
+        try {
+            sklep.obsluzKlientow(klienci);
+        } catch (ZlodziejWSklepieException e) {
+            System.out.println("Zlodziej w sklepie! Ewakuacja!");
+        }
     }
 }
